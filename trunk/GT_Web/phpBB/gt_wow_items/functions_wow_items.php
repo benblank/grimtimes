26c0c4e7577b30a8b152f745974fe0d50d19222e
dark.ryder <dark.ryder@f803c75a-e419-0410-8599-1b76a3dede3a>
2006-09-19 23:49:41 +0000

    Caching nearly complete; now also working on bbcode_first_pass.

--- trunk/GT_Web/phpBB/gt_wow_items/functions_wow_items.php
+++ trunk/GT_Web/phpBB/gt_wow_items/functions_wow_items.php
@@ -10,28 +10,28 @@ function wow_item_clean($html) {
 	static $wow_item_clean;
 
 	if (!isset($wow_item_clean)) {
-		$wow_item_clean['&lt;']  = '<';
-		$wow_item_clean['&gt;']  = '>';
-		$wow_item_clean['&quot;']  = '"';
-		$wow_item_clean['&amp;']  = '&';
-		$wow_item_clean['\s*(?:&nbsp;)*\s*<br\s*/>\s*(?:&nbsp;)*\s*']  = '<br />';
-		$wow_item_clean['[\r\n]+']  = '';
-		$wow_item_clean['<span class="iname"><span class="([^"]+)">(.+)</span></span>']  = '<span class="itemname \1">\2</span>';
-		$wow_item_clean['gr[ae]yname']  = 'quality0';
-		$wow_item_clean['whitename']  = 'quality1';
-		$wow_item_clean['greenname']  = 'quality2';
-		$wow_item_clean['bluename']  = 'quality3';
-		$wow_item_clean['purplename']  = 'quality4';
-		$wow_item_clean['orangename']  = 'quality5';
-		$wow_item_clean['redname']  = 'quality6';
-		$wow_item_clean['wowrttxt']  = 'right';
-		$wow_item_clean['(<span class="itemeffect)link']  = '\1';
-		$wow_item_clean['(<a[^>]*) class="itemeffectlink"']  = '\1';
-		$wow_item_clean['(<a[^>]*)><span class="goldtext">(.+)</span></a>']  = '\1 class="itemdesc">\2</a>';
-		$wow_item_clean['<span class="akznotice">(.+)</span>']  = '\1';
+		$wow_item_clean['#&lt;#'] = '<';
+		$wow_item_clean['#&gt;#'] = '>';
+		$wow_item_clean['#&quot;#'] = '"';
+		$wow_item_clean['#&amp;#'] = '&';
+		$wow_item_clean['#\s*(?:&nbsp;)*\s*<br\s*/>\s*(?:&nbsp;)*\s*#'] = '<br />';
+		$wow_item_clean['#[\r\n]+#'] = '';
+		$wow_item_clean['#<span class="iname"><span class="([^"]+)">(.+?)</span>(.*?)</span>#'] = '<span class="itemname \1">\2\3</span>';
+		$wow_item_clean['#gr[ae]yname#'] = 'quality0';
+		$wow_item_clean['#whitename#'] = 'quality1';
+		$wow_item_clean['#greenname#'] = 'quality2';
+		$wow_item_clean['#bluename#'] = 'quality3';
+		$wow_item_clean['#purplename#'] = 'quality4';
+		$wow_item_clean['#orangename#'] = 'quality5';
+		$wow_item_clean['#redname#'] = 'quality6';
+		$wow_item_clean['#wowrttxt#'] = 'right';
+		$wow_item_clean['#(<span class="itemeffect)link#'] = '\1';
+		$wow_item_clean['#(<a[^>]*) class="itemeffectlink"#'] = '\1';
+		$wow_item_clean['#(<a[^>]*)><span class="goldtext">(.+)</span></a>#'] = '\1 class="itemdesc">\2</a>';
+		$wow_item_clean['#<span class="akznotice">(.+)</span>#'] = '\1';
 	}
 
-	return preg_replace($html, array_keys($wow_item_clean), array_values($wow_item_clean));
+	return preg_replace(array_keys($wow_item_clean), array_values($wow_item_clean), $html);
 }
 
 function wow_item_search($search) {
@@ -46,8 +46,8 @@ function wow_item_search($search) {
 		$sql .= " AND item_desc LIKE '%{$params[1]}%'";
 	}
 
-	$result = $db->sql_query($sql);
-	$result = $db->sql_fetchrowset($result);
+	if (!($result = $db->sql_query($sql))) return array();
+	if (!($result = $db->sql_fetchrowset($result))) return array();
 
 	foreach ($result as $k =>$v) $result[$k] = $v['item_id'];
 	sort($result);
@@ -56,7 +56,7 @@ function wow_item_search($search) {
 }
 
 function wow_item_cache_map() {
-	global $db;
+	wow_item_cache_item(0);
 }
 
 function wow_item_cache_item($item) {
@@ -69,29 +69,45 @@ function wow_item_cache_item($item) {
 	$script_name = preg_replace('#^\/?(.*?)\/?$#', '\1', trim($board_config['script_path']));
 	$script_name = ($script_name == '') ? $script_name : '/' . $script_name;
 
-	// The wow_item_cache script uses ignore_user_abort() to "asynchonously" cache items in the background.
-	$handle = fopen($server_protocol . $server_name . $server_port . $script_name . "wow_item_cache.$phpEx?item=$item", 'r');
-	fclose($handle);
-}
+	if (gettype($item) == "array") {
+		// Do array stuff
+	} else if (is_numeric($item)) {
+		// Do numeric stuff
+	} else return false;
 
-function wow_item_cache_items($items) {
-	foreach ($items as $v) wow_item_cache_item($v);
+	// The wow_items_cache script uses ignore_user_abort() to "asynchonously" cache items in the background.
+	$handle = fopen($server_protocol . $server_name . $server_port . $script_name . "wow_items_cache.$phpEx?item=$item", 'r');
+	fclose($handle);
+
+	// We may not know whether it worked or not, but we successfully made the request.
+	return true;
 }
 
 function wow_item_get_info($itemnum) {
-	$fetch_url = "http://wow.allakhazam.com/dev/wow/item-xml.pl?witem=" + $item_num;
+	if (!is_numeric($itemnum)) return false;
+	if (!($result = $db->sql_query("SELECT * FROM " . WOW_ITEMS_TABLE . " WHERE item_id = " . intval($itemnum)))) return false;
+	if (!($result = $db->sql_fetchrow($result))) return false;
+	return $result;
 }
 
 function wow_item_bbcode_first_pass($text, $uid) {
 	$result = $text;
 
 	$callback = create_function('$matches', '
-		$uid = ' . $uid . ';
-		$q = 1;
-		$sql = "SELECT item_quality FROM " . WOW_ITEMS_TABLE . " WHERE item_id = " . $id;
+		$uid = ' . $uid . '; // This is the *only* reason we have to use create_function.  Arrgh!
+
+		if ($matches[2]
+		$found = wow_item_search($matches[3]);
+		if (!$found || count($found) != 1) {
+			$item_quality = 1;
+		} else {
+			$found = 
+			$item_id = $found[0]['item_id'];
+			$item_quality = $found[0]['item_quality']
+		}
 	');
 
-	preg_replace_callback('#\[item((?:desc)?)\]([^\[]+)\[/item(?:desc)?\]#is', $callback, $text);
+	preg_replace_callback('#\[item((?:desc)?)((?:=\d+)?)\]([^\[]+)\[/item(?:desc)?\]#is', $callback, $text);
 }
 
 function wow_item_bbcode_second_pass($text, $uid) {
---
+++ trunk/GT_Web/phpBB/gt_wow_items/wow_items_cache.php
@@ -0,0 +1,134 @@
+<?php /* $Id$ */
+
+	define('IN_PHPBB', true);
+
+	// Prevent caching from being interrupted by silly little things like
+	// closed connections and slow responses from Allakhazam.
+	ignore_user_abort(true);
+	set_time_limit(0);
+
+	// Just enough to get the DBAL working...
+	$phpbb_root_path = './';
+	include($phpbb_root_path . 'extension.inc');
+	include($phpbb_root_path . 'config.'.$phpEx);
+	include($phpbb_root_path . 'includes/db.'.$phpEx);
+	unset($dbpasswd);
+
+	include($phpbb_root_path . 'includes/functions_wow_items.'.$phpEx);
+
+	$pending = "This item has not yet been cached.  It is possible that Allakhazam is down or that the item ID is invalid.  Please try again later.";
+
+	function die_text($msg, $line) {
+		// "Magic" database errors.
+		if (gettype($msg) == "array") $msg = "Database error {$msg['code']}: \"{$msg['message']}\"";
+		else if (gettype($msg) != "string") $msg = "Error";
+		else $msg = "Error \"$msg\"";
+
+		die($msg . " on line " . $line);
+	}
+
+	$items = array();
+
+	if (isset($_POST) && (isset($_POST['item']) || isset($_POST['items']))) {
+		$req = &$_POST;
+	} else if (isset($_GET) && (isset($_GET['item']) || isset($_GET['items']))) {
+		$req = &$_GET;
+	} else if (isset($HTTP_POST_VARS) && (isset($HTTP_POST_VARS['item']) || isset($HTTP_POST_VARS['items']))) {
+		$req = &$HTTP_POST_VARS;
+	} else if (isset($HTTP_GET_VARS) && (isset($HTTP_GET_VARS['item']) || isset($HTTP_GET_VARS['items']))) {
+		$req = &$HTTP_GET_VARS;
+	} else if (!isset($argc)) {
+		if (isset($_SERVER) && isset($_SERVER['argc'])) {
+			$argc = &$_SERVER['argc'];
+			$argv = &$_SERVER['argv'];
+		} else if (isset($HTTP_SERVER_VARS) && isset($HTTP_SERVER_VARS['argc'])) {
+			$argc = &$HTTP_SERVER_VARS['argc'];
+			$argv = &$HTTP_SERVER_VARS['argv'];
+		}
+	}
+
+	if (isset($req)) {
+		if (isset($req['item']) && preg_match('/^-?\d+$/', $req['item'])) $items[] = intval($req['item']);
+		if (isset($req['items'])) foreach ($req['items'] as $v) if (preg_match('/^-?\d+$/', $v)) $items[] = intval($v);
+	} else if (isset($argc)) {
+		for ($i = 1; $i < $argc - 1; $i++) if (preg_match('/^-?\d+$/', $argv[$i])) $items[] = intval($argv[$i]);
+	} else {
+		die_text("Could not find POST request, GET request, or command line parameters", __LINE__);
+	}
+
+	if (count($items) == 0) die_text("No items specified; make sure you are using item IDs", __LINE__);
+
+	// Item "zero" means rebuild the ID <-> Name mapping
+	if (in_array(0, $items) || in_array(-1, $items)) {
+		// Item -1 is the Big Cache, so don't bother collecting the names of cached items.
+		if (!in_array(-1, $items)) {
+			// Get a list of which items need to be re-cached.  We use
+			// item_desc instead of item_stamp because the latter is used
+			// to track when the mapping was last updated.
+			$result = $db->sql_query("SELECT item_id FROM " . WOW_ITEMS_TABLE . " WHERE item_desc IS NOT NULL", BEGIN_TRANSACTION);
+			if (!$result) die_text($db->sql_error(), __LINE__);
+			$result = $db->sql_fetchrowset($result);
+			if ($result) foreach ($result as $v) $items[] = intval($v['item_id']);
+		}
+
+		// And here's why we're doing this transactionally.  Ouch!
+		// Pass BEGIN_TRANSACTION again, as the first section is skipped on item -1...
+		if (!$db->sql_query("TRUNCATE " . WOW_ITEMS_TABLE, BEGIN_TRANSACTION)) die_text($db->sql_error(), __LINE__);
+
+		if (!($lines = file("http://wow.allakhazam.com/itemlist.xml"))) die_text("Failure reading item list from Allakhazam", __LINE__);
+
+		$rows = array("(0, '00index', NULL, CURRENT_TIMESTAMP())");
+
+		foreach ($lines as $line) if (preg_match('#<wowitem\s+name="([^"]+)"\s+id="(\d+)"\s*/>#', $line, $match) && $match[1]{0} != "ÿ") {
+			$id = intval($match[2]);
+			$rows[] = "($id,'" . str_replace("'", "''", $match[1]) . "'," . (in_array($id, $items) ? "'$pending'" : "NULL") . ",NULL)";
+		}
+
+		$sql = "INSERT INTO " . WOW_ITEMS_TABLE . " (item_id, item_name, item_desc, item_stamp)
+		        VALUES " . implode(",", $rows);
+
+		if (!($db->sql_query($sql, END_TRANSACTION))) die_text($db->sql_error(), __LINE__);
+	}
+
+	if (!in_array(-1, $items)) {
+		// Make sure we're not duplicating effort.
+		$items = array_unique($items);
+	} else {
+		// Get the Big Cache list.
+		if (!($result = $db->sql_query("SELECT item_id FROM " . WOW_ITEMS_TABLE))) die_text($db->sql_error(), __LINE__);
+		if (!($result = $db->sql_fetchrowset($result)
+	}
+
+	$rows = array();
+
+	// Any kind of failure in this loop causes a continue instead of an abort;
+	// no need to throw it all away because one item is bad.
+	foreach ($items as $id) {
+		if ($id == 0) continue; // Ignore item "zero".
+
+		if (!($lines = file("http://wow.allakhazam.com/dev/wow/item-xml.pl?witem=$id"))) continue;
+		$xml = implode("", $lines);
+
+		if (!preg_match('#<id>(\d+)</id>#', $xml, $match) || $match[1] != $id) continue;
+
+		if (!preg_match('#<display_html>(.+?)</display_html>#s', $xml, $match)) continue;
+		$desc = $match[1];
+
+		if (!preg_match('#<icon>(.+?)</icon>#', $xml, $match)) continue;
+		$icon = $match[1];
+
+		if (!preg_match('#<name1>(.+?)</name1>#', $xml, $match)) continue;
+		$name = $match[1];
+
+		if (!preg_match('#<quality>(.+?)</quality>#', $xml, $match)) continue;
+		$quality = intval($match[1]);
+
+		$rows[] = "($id,'"  . str_replace("'", "''", $name) . "',$quality,'" . str_replace("'", "''", $icon) . "',CURRENT_TIMESTAMP(),'" . str_replace("'", "''", wow_item_clean($desc)) . "')";
+	}
+
+	if (count($rows) > 0) {
+		$sql = "REPLACE INTO " . WOW_ITEMS_TABLE . " (item_id,item_name,item_quality,item_icon,item_stamp,item_desc)
+		        VALUES " . implode(",", $rows);
+
+		if (!$db->sql_query($sql)) die_text($db->sql_error(), __LINE__);
+	}
