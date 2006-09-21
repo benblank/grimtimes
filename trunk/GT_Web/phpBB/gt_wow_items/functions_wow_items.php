42f5382f8847b801e871a36a9a8596bf4f06d44a
dark.ryder <dark.ryder@f803c75a-e419-0410-8599-1b76a3dede3a>
2006-09-21 14:22:08 +0000

    Almost there!  Just need to debug the second pass logic and javascript and figure out why the stylesheet isn't applying.

--- trunk/GT_Web/phpBB/gt_wow_items/functions_wow_items.php
+++ trunk/GT_Web/phpBB/gt_wow_items/functions_wow_items.php
@@ -60,7 +60,7 @@ function wow_item_cache_map() {
 }
 
 function wow_item_cache_item($item) {
-	global $board_config, $phpEx;
+	global $board_config, $db, $lang, $phpEx;
 
 	// Copied from redirect() in includes/functions.php
 	$server_protocol = ($board_config['cookie_secure']) ? 'https://' : 'http://';
@@ -71,12 +71,23 @@ function wow_item_cache_item($item) {
 
 	if (gettype($item) == "array") {
 		$query = array();
-		foreach ($item as $v) if (is_numeric($v)) $query[] = "items[]=" . intval($v);
+		$sql = array();
+
+		$item = array_unique($item);
+		foreach ($item as $v) if (is_numeric($v)) {
+			$query[] = "items[]=" . intval($v);
+			$sql[] = intval($v);
+		}
 		if (count($query) == 0) return false;
-		$query = implode("&", array_unique($query));
+
+		$query = implode("&", $query);
+		$sql = "IN (" . implode(",", $sql) . ")";
 	} else if (is_numeric($item)) {
 		$query = "item=" . intval($item);
+		$sql = "= " . intval($item);
 	} else return false;
+
+	$db->sql_query("UPDATE " . WOW_ITEMS_TABLE . " SET item_desc='" . str_replace("'", "''", $lang['wow_items_pending']) . "' WHERE item_desc IS NULL AND item_id $sql");
 
 	// The wow_items_cache script uses ignore_user_abort() to "asynchonously" cache items in the background.
 	$handle = fopen($server_protocol . $server_name . $server_port . $script_name . "/wow_items_cache.$phpEx?$query", 'r');
@@ -87,6 +98,8 @@ function wow_item_cache_item($item) {
 }
 
 function wow_item_get_info($itemnum) {
+	global $db;
+
 	if (!is_numeric($itemnum)) return false;
 	if (!($result = $db->sql_query("SELECT * FROM " . WOW_ITEMS_TABLE . " WHERE item_id = " . intval($itemnum)))) return false;
 	if (!($result = $db->sql_fetchrow($result))) return false;
@@ -94,21 +107,85 @@ function wow_item_get_info($itemnum) {
 }
 
 function wow_item_bbcode_first_pass($text) {
-	preg_match_all('#\[item(?:desc)?((?:=\d+)?)\]([^\[]+)\[/item(?:desc)?\]#is', $text, $matches, PREG_SET_ORDER);
+	preg_match_all('#\[item(?:desc)?((?:=[1-9]\d*)?)\]([^\[]+)\[/item(?:desc)?\]#is', $text, $matches, PREG_SET_ORDER);
 
 	// There's no way to gather useful info ahead of time on non-cached items,
 	// so we'll cache them on the first pass and process on the second.
 	foreach($matches as $match) {
 		if ($match[1]) {
 			wow_item_cache_item(intval(substr($match[1], 1)));
-		} else if (preg_match('/^\d+$/', $match[2])) {
+		} else if (preg_match('/^[1-9]\d*$/', $match[2])) {
 			wow_item_cache_item(intval($match[2]));
 		} else {
+			// For items with description matches (e.g. "Eldre'Thalas
+			// (Warlock)"), the part in parentheses is ignored during the first
+			// pass, to ensure that the data is available to the second pass.
 			wow_item_cache_item(wow_item_search(str_replace("\\'", "'", $match[2]), false));
 		}
 	}
 }
 
-function wow_item_bbcode_second_pass($text, $bbcode_tpl) {
-	return $text;
+function wow_item_bbcode_second_pass($text) {
+	global $wibspcd;
+
+	return preg_replace_callback('#\[item((?:desc)?)((?:=[1-9]\d*)?)\]([^\[]+)\[/item(?:desc)?\]#is', "wow_item_bbcode_second_pass_callback", $text);
+}
+
+$wibspcd = array();
+
+function wow_item_bbcode_second_pass_callback($match) {
+	global $bbcode_tpl, $lang, $wibspcd;
+
+	// Determine whether there is a single-item match.
+	if ($match[2]) {
+		$id = intval(substr($match[2], 1));
+	} else if (preg_match("/^[1-9]\d*$/", $match[3])) {
+		$id = intval($match[3]);
+	} else {
+		$search = wow_item_search(str_replace("\\'", "'", $match[3]));
+		if (count($search) == 1) $id = $search[0];
+	}
+
+	if ($id && ($info = wow_item_get_info($id))) {
+		$url = "http://wow.allakhazam.com/db/item.html?witem=$id";
+		$itemdesc = $info['item_desc'] ? $info['item_desc'] : $lang['wow_items_pending'];
+		$quality = $info['item_quality'] ? $info['item_quality'] : 99;
+		if ($match[2]) $text = $match[3];
+		else $text = $info['item_name'];
+	} else if (count($search) > 1) {
+		$text = $match[3];
+		$url = "http://wow.allakhazam.com/search.html?q=" . urlencode($text);
+		$id = md5($text);
+		$quality = 1;
+		$itemdesc = str_replace(array("{SEARCH}", "{COUNT}"), array($text, count($search)), $lang['wow_items_multi']) . "<br />";
+		foreach ($search as $v) $itemdesc .= "<br />[item]{$v}[/item]";
+		$itemdesc = wow_item_bbcode_second_pass($itemdesc); // A dirty trick, but...
+	} else if ($id) {
+		// An item ID was found, but it did not return data.
+		$text = $match[3];
+		$url = "http://wow.allakhazam.com/search.html?q=" . urlencode($text);
+		$quality = 99;
+		$itemdesc = str_replace("{ID}", $id, $lang['wow_item_badid']);
+	} else {
+		$text = $match[3];
+		$url = "http://wow.allakhazam.com/search.html?q=" . urlencode($text);
+		$id = md5($text);
+		$quality = 99;
+		$itemdesc = str_replace("{SEARCH}", $text, $lang['wow_item_none']);
+	}
+
+	if (!in_array($id, $wibspcd)) {
+		$wibspcd[] = $id;
+		$idattr = " id=\"item$id\"";
+		if (!$match[1]) $itemdiv = str_replace(array("{IDATTR}", "{ITEMDESC}"), array($idattr, $itemdesc), $bbcode_tpl['itemdiv']);
+	} else {
+		$itemdiv = "";
+		$idattr = "";
+	}
+
+	if ($match[1]) {
+		return str_replace(array("{IDATTR}", "{ITEMDESC}"), array($idattr, $itemdesc), $bbcode_tpl['itemdesc']);
+	} else {
+		return str_replace(array("{ITEMDIV}", "{QUALITY}", "{ID}", "{URL}", "{TEXT}"), array($itemdiv, $quality, $id, $url, $text), $bbcode_tpl['item']);
+	}
 }
