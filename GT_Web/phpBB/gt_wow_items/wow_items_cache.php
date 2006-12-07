f69b16dde5f32d05d22148066a17cbd8a6e8b847
dark.ryder <dark.ryder@f803c75a-e419-0410-8599-1b76a3dede3a>
2006-12-07 00:25:48 +0000

    Partial rewrite of gt_wow_items' caching system.  Index rebuilds and item recaches are now automatic and should have a lower impact on perfomance.

--- GT_Web/phpBB/gt_wow_items/wow_items_cache.php
+++ GT_Web/phpBB/gt_wow_items/wow_items_cache.php
@@ -1,5 +1,9 @@
 <?php /* $Id$ */
 
+	// When called via the command line or from a script on the same server (a
+	// la functions_wow_items.php), the local and remote addresses are the same.
+	if ($_SERVER['SERVER_ADDR'] != $_SERVER['REMOTE_ADDR']) die("Hacking attempt!");
+
 	define('IN_PHPBB', true);
 
 	// Prevent caching from being interrupted by silly little things like
@@ -15,9 +19,39 @@
 	include($phpbb_root_path . 'includes/db.'.$phpEx);
 	unset($dbpasswd);
 
-	include($phpbb_root_path . 'includes/functions_wow_items.'.$phpEx);
+	$stamp = time();
+	$limit = $stamp - 2592000; // Cache expires after 30 days
+	$table = $table_prefix . 'wow_items';
 
-	$pending = "This item has not yet been cached.  It is possible that Allakhazam is down or that the item ID is invalid.  Please try again later.";
+	// Clean up silliness in Allak's HTML
+	function clean_item($html) {
+		static $wow_item_clean;
+
+		if (!isset($wow_item_clean)) {
+			$wow_item_clean['#&lt;#'] = '<';
+			$wow_item_clean['#&gt;#'] = '>';
+			$wow_item_clean['#&quot;#'] = '"';
+			$wow_item_clean['#&amp;#'] = '&';
+			$wow_item_clean['#&nbsp;#'] = ' ';
+			$wow_item_clean['#\s*(?:&nbsp;)*\s*<br\s*/>\s*(?:&nbsp;)*\s*#'] = '<br />';
+			$wow_item_clean['#[\r\n]+#'] = '';
+			$wow_item_clean['#<span class="iname"><span class="([^"]+)">(.+?)</span>(.*?)</span>#'] = '<span class="itemname \1">\2\3</span>';
+			$wow_item_clean['#gr[ae]yname#'] = 'quality0';
+			$wow_item_clean['#whitename#'] = 'quality1';
+			$wow_item_clean['#greenname#'] = 'quality2';
+			$wow_item_clean['#bluename#'] = 'quality3';
+			$wow_item_clean['#purplename#'] = 'quality4';
+			$wow_item_clean['#orangename#'] = 'quality5';
+			$wow_item_clean['#redname#'] = 'quality6';
+			$wow_item_clean['#wowrttxt#'] = 'right';
+			$wow_item_clean['#(<span class="itemeffect)link#'] = '\1';
+			$wow_item_clean['#(<a[^>]*) class="itemeffectlink"#'] = '\1';
+			$wow_item_clean['#(<a[^>]*)><span class="goldtext">(.+)</span></a>#'] = '\1 class="itemdesc">\2</a>';
+			$wow_item_clean['#goldtext#'] = 'itemdesc';
+		}
+
+		return preg_replace(array_keys($wow_item_clean), array_values($wow_item_clean), $html);
+	}
 
 	function die_text($msg, $line) {
 		// "Magic" database errors.
@@ -28,6 +62,7 @@
 		die($msg . " on line " . $line);
 	}
 
+	// Fall back to CURL if fopen wrappers aren't working.
 	function url($url) {
 		$result = @file($url);
 		if (false !== $result) return $result;
@@ -71,39 +106,46 @@
 
 	if (count($items) == 0) die_text("No items specified; make sure you are using item IDs", __LINE__);
 
-	// Item "zero" means rebuild the ID <-> Name mapping
-	if (in_array(0, $items)) {
-		// Get a list of which items need to be re-cached.  We use
-		// item_desc instead of item_stamp because the latter is used
-		// to track when the mapping was last updated.
-		$result = $db->sql_query("SELECT item_id FROM " . WOW_ITEMS_TABLE . " WHERE item_desc IS NOT NULL", BEGIN_TRANSACTION);
-		if (!$result) die_text($db->sql_error(), __LINE__);
-		$result = $db->sql_fetchrowset($result);
-		if ($result) foreach ($result as $v) $items[] = intval($v['item_id']);
-
-		// And here's why we're doing this transactionally.  Ouch!
-		if (!$db->sql_query("TRUNCATE " . WOW_ITEMS_TABLE)) die_text($db->sql_error(), __LINE__);
-
-		if (!($lines = url("http://wow.allakhazam.com/itemlist.xml"))) die_text("Failure reading item list from Allakhazam", __LINE__);
-
-		$rows = array("(0, '00index', NULL)");
-
-		foreach ($lines as $line) if (preg_match('#<wowitem\s+name="([^"]+)"\s+id="(\d+)"\s*/>#', $line, $match) && $match[1]{0} > "\x1F" && $match[1]{0} < "\x7F") {
-			$id = intval($match[2]);
-			$rows[] = "($id,'" . str_replace("'", "''", $match[1]) . "'," . (in_array($id, $items) ? "'<div class=\"wowitem\">$pending</div>')" : "NULL)");
-		}
-
-		$sql = "INSERT INTO " . WOW_ITEMS_TABLE . " (item_id, item_name, item_desc)
-		        VALUES " . implode(",", $rows);
-
-		if (!($db->sql_query($sql, END_TRANSACTION))) die_text($db->sql_error(), __LINE__);
+	if (!($result = $db->sql_query("SELECT item_stamp FROM $table WHERE item_id = 0"))) die_text($db->sql_error(), __LINE__);
+	if ($db->sql_numrows($result) == 0) {
+		if (!($result = $db->sql_query("INSERT INTO $table (item_id, item_stamp, item_name) VALUES (0, 0, '00index')"))) die_text($db->sql_error(), __LINE__);
+		$items[] = 0;
+	} else {
+		if (!($result = $db->sql_fetchrowset($result))) die_text($db->sql_error(), __LINE__);
+		if (intval($result[0]['item_stamp']) < $limit) $items[] = 0;
 	}
 
-	// Only cache items we don't already have.
-	if (!($result = $db->sql_query("SELECT item_id FROM " . WOW_ITEMS_TABLE . " WHERE item_icon IS NULL"))) die_text($db->sql_error(), __LINE__);
+	// Update the ID <--> Name mapping if it is missing or out of date
+	if (in_array(0, $items)) {
+		if (!($lines = url("http://wow.allakhazam.com/itemlist.xml"))) die_text("Failure reading item list from Allakhazam", __LINE__);
+
+		$ids = array();
+		$rows = array();
+		foreach ($lines as $line) if (preg_match('#<wowitem\s+name="([^"]+)"\s+id="(\d+)"\s*/>#', $line, $match) && $match[1]{0} > "\x1F" && $match[1]{0} < "\x7F") {
+			$id = intval($match[2]);
+			$good_ids[] = $id;
+			$rows[] = "($id, 0, '" . str_replace("'", "''", $match[1]) . "')";
+		}
+
+		if (!($result = $db->sql_query("SELECT item_id FROM $table"))) die_text($db->sql_error(), __LINE__);
+		if ($db->sql_numrows($result) > 0) {
+			if (!($result = $db->sql_fetchrowset($result))) die_text($db->sql_error(), __LINE__);
+			$bad_ids = array();
+			foreach ($result as $v) $bad_ids[] = intval($v['item_id']);
+			$bad_ids = array_diff($bad_ids, $good_ids, array(0));
+			if (count($bad_ids) > 0) if (!($db->sql_query("DELETE FROM $table WHERE item_id IN (" . implode(",", $bad_ids) . ")"))) die_text($db->sql_error(), __LINE__);
+		}
+
+		if (!($db->sql_query("INSERT INTO $table (item_id, item_stamp, item_name) VALUES " . implode(",", $rows) . " ON DUPLICATE KEY UPDATE item_name=VALUES(item_name), item_stamp=IF(item_name = VALUES(item_name), item_stamp, 0)"))) die_text($db->sql_error(), __LINE__);
+		if (!($db->sql_query("UPDATE $table SET item_stamp=$stamp WHERE item_id = 0"))) die_text($db->sql_error(), __LINE__);
+	}
+
+	// Only cache items which aren't already up to date
+	if (!($result = $db->sql_query("SELECT item_id FROM $table WHERE item_stamp < $limit"))) die_text($db->sql_error(), __LINE__);
 	if (!($result = $db->sql_fetchrowset($result))) die_text($db->sql_error(), __LINE__);
-	foreach ($result as $k => $v) $result[$k] = $v['item_id'];
-	$items = array_intersect(array_unique($items), $result);
+	$rows = array();
+	foreach ($result as $v) $rows[] = $v['item_id'];
+	$items = array_intersect(array_unique($items), $rows);
 
 	$rows = array();
 
@@ -129,11 +171,11 @@
 		if (!preg_match('#<quality>(.+?)</quality>#', $xml, $match)) continue;
 		$quality = intval($match[1]);
 
-		$rows[] = "($id,'"  . str_replace("'", "''", $name) . "',$quality,'" . str_replace("'", "''", $icon) . "','" . str_replace("'", "''", wow_item_clean($desc)) . "')";
+		$rows[] = "($id,$stamp,'" . str_replace("'", "''", $name) . "',$quality,'" . str_replace("'", "''", $icon) . "','" . str_replace("'", "''", clean_item($desc)) . "')";
 	}
 
 	if (count($rows) > 0) {
-		$sql = "REPLACE INTO " . WOW_ITEMS_TABLE . " (item_id,item_name,item_quality,item_icon,item_desc)
+		$sql = "REPLACE INTO $table (item_id,item_stamp,item_name,item_quality,item_icon,item_desc)
 		        VALUES " . implode(",", $rows);
 
 		if (!$db->sql_query($sql)) die_text($db->sql_error(), __LINE__);
